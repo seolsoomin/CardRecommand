a917adbf01158175f6b5a0b67ce7248be283d41d
SonHyunYoung <son2497474@gmail.com>
2025-11-24 23:11:36 +0900

    Update maria.js

--- public/maria.js
+++ public/maria.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host : '192.168.193.1',
+    host : '192.168.0.44',
     user : 'remote_user',
     port : 3307,
     password : '1234',
